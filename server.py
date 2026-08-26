51215bb4b16bed801304689e747ac1e2d922166c
Pablo Henrique Almeida Mendes <pablo.mendes@ifood.com.br>
2026-08-26 09:46:51 -0300

    chore: running tests

--- server.py
+++ server.py
@@ -1,32 +1,46 @@
 from socket import *
 import sys # In order to terminate the program
+HOST = ''
+PORT = 50007
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
+s = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
-#Fill in start
-#Fill in end
-while True:
-    #Establish the connection
-    print('Ready to serve...')
-    connectionSocket, addr = null #Fill in start #Fill in end
-    try:
-        message = null #Fill in start #Fill in end
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = null #Fill in start #Fill in end
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-    except IOError:
-        #Send response message for file not found
-        #Fill in start#Fill in end
-        #Close client socket
-        #Fill in start
-        #Fill in end
-    serverSocket.close()
+
+s.bind((HOST,PORT))
+s.listen(1)
+print('Ready to serve...')
+conn, addr = s.accept()
+
+with conn:
+    print(f'Connection established {conn}')
+
+    while True:
+        #Establish the connection
+
+        try:
+            conn.sendall(b'Hello World')
+            message = conn.recv(1024)
+            if not message: break
+            print('Received', repr(message))
+            # filename = message.split()[1]
+            # f = open(filename[1:])
+            # output_data = f
+            #Send one HTTP header line into socket
+
+
+            #Send the content of the requested file to the client
+
+            # for i in range(0, len(outputdata)):
+            #    conn.send(outputdata[i].encode())
+            # conn.send("\r\n".encode())
+
+            conn.close()
+        except IOError:
+            #Send response message for file not found
+            print(f'Unexpected error: {IOError}')
+
+            #Close client socket
+
+
+    s.close()
     sys.exit()#Terminate the program after sending the corresponding data
